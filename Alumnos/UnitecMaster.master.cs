e9b7db770d12ef64682f016f23b4dcf82bd11ef7
BUSESJOR <betcoy77@hotmail.com>
2016-09-05 21:29:06 -0600

    Alumnos  Login y btn Salir
    Login y btn Salir

--- Alumnos/UnitecMaster.master.cs
+++ Alumnos/UnitecMaster.master.cs
@@ -4,11 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Unitec.CRUD.Business;
+using Unitec.CRUD.Business.Entity;
 
 public partial class UnitecMaster : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+    protected void lnkLogin_Click(object sender, EventArgs e)
+    {
+        EntUsuario ent = new BusAlumno().ValidarAlumno(txtMail.Text, txtPassword.Text);
+        if (ent != null)
+        {
+            lblUsuario.Text = "Bienvenido" + ent.nombre;
+            lnkEntrar.Text = "";
+        }
+        else
+            lnkEntrar.Text = "Login";
+
+    }
+    protected void lnkSalir_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Session.Clear();
+            Response.Redirect("Index.aspx");
+        }
+        catch (Exception ex)
+        {
+            
+            MostrarMensage(ex.Message);
+        }
+    }
+
+    private void MostrarMensage(string mensaje)
+    {
+        string alerta = mensaje.Replace("\n","").Replace("\r","").Replace("'","");
+        alerta = "alert('" + alerta + "');";
+        ScriptManager.RegisterStartupScript(this, GetType(), "", alerta, true);
     }
 }
